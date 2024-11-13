8a6a0e36083f4d275060f00066a9f972cfa61c19
Vitaliy-Avdeev <avd.vit78@gmail.com>
2024-11-13 15:45:08 +0300

    Corrected the task

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,55 @@
 public class Main {
-    public static void checkYears(int year) {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int year = 2024;
+        calculateLeapYear(year);
         if (year < 1584) {
             System.out.println("Високосный год ввели после этого года");
-        } else if (year % 400 == 0 || year % 4 == 0) {
-            System.out.println(year + " год високосный");
-        } else if (year % 100 != 0) {
-            System.out.println(year + " год невисокосный");
+        } else {
+            System.out.println(calculateLeapYear(year));
+
+        }
+
+        int osType = 1;
+        int deviceYear = 2015;
+        calculateDeviceVersion(osType, deviceYear);
+        if (osType > 1) {
+            System.out.println(" ");
+        } else {
+            System.out.println(calculateDeviceVersion(osType, deviceYear));
+        }
+
+        int deliveryDistance = 95;
+        int days = distanceTime(deliveryDistance);
+        if (days > 0) {
+            System.out.println("Потребуется дней: " + days + " срок доставки.");
+        } else {
+            System.out.println("Доставки нет.");
+        }
+
+    }
+
+    public static String calculateLeapYear(int year) {
+        if (year > 1584 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
+            return year + " год - високосный";
+        } else {
+            return year + " год - невисокосный";
         }
     }
 
-    public static void checkerDeviceClient(int clientDeviceOs, int currentYear) {
-        boolean clientOsIos = clientDeviceOs == 0;
-        boolean clientOsAndroid = clientDeviceOs == 1;
-        boolean year = currentYear < 2015;
-        switch (clientDeviceOs) {
-            case 0:
-                System.out.println("Установите приложение для IOS");
-                break;
-            case 1:
-                System.out.println("Установите приложение для Android");
-                break;
-            default:
-                System.out.println("Некорректность введенного значения");
-        }
-        if (clientOsIos && year) {
-            System.out.println("Установите облегченную версию приложения для iOS");
-        } else if (clientOsAndroid && year) {
-            System.out.println("Установите облегченную версию приложения для Android");
+    public static String calculateDeviceVersion(int osType, int deviceYear) {
+        if (osType == 1) {
+            if (deviceYear <= 2015) {
+                return "Установите облегченную версию приложения для Android по ссылке";
+            } else {
+                return "Установите версию приложения для Android по ссылке";
+            }
+        } else {
+            if (deviceYear <= 2015) {
+                return "Установите облегченную версию приложения для iOS по ссылке";
+            } else {
+                return "Установите версию приложения для iOS по ссылке";
+            }
         }
     }
 
@@ -39,23 +62,8 @@ public class Main {
             return 3;
         } else {
             return 0;
-        }
-    }
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int year = 2024;
-        checkYears(year);
-        int clientDeviceOs = 0;
-        int currentYear = 2015;
-        checkerDeviceClient(clientDeviceOs, currentYear);
-        int deliveryDistance = 95;
-        int days = distanceTime(deliveryDistance);
-        if (days > 0) {
-            System.out.println("Потребуется дней: " + days + " срок доставки.");
-        } else {
-            System.out.println("Доставки нет.");
-        }
 
+        }
     }
 }
